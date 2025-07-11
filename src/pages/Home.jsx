a2a428ce196a227a6adce965bd376ebe9a7bace4
Daojim <jimmyddao@gmail.com>
2025-07-11 19:27:25 -0400

    Change from StarBackground to BackgroundBeams

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,24 +1,29 @@
 import { ThemeToggle } from "../components/ThemeToggle";
-import { StarBackground } from "../components/StarBackground";
 import { Navbar } from "../components/Navbar";
 import { HeroSection } from "../components/HeroSection";
 import { AboutSection } from "../components/AboutSection";
+import { BackgroundBeams } from "../components/ui/BackgroundBeams";
+import { TracingBeam } from "../components/ui/TracingBeam";
 
 export const Home = () => {
   return (
     <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
       {/* {Theme Toggle} */}
       <ThemeToggle />
+
       {/* {Background Effects} */}
-      <StarBackground />
+
+      <BackgroundBeams />
+
       {/* {Navbar} */}
       <Navbar />
       {/* {Main Content} */}
-      <main>
-        <HeroSection />
-        <AboutSection />
-      </main>
-
+      <TracingBeam>
+        <main>
+          <HeroSection />
+          <AboutSection />
+        </main>
+      </TracingBeam>
       {/* {Footer} */}
     </div>
   );
