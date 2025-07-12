4008f9111506cfaaa7beb589c5b390ca52c22ba9
Daojim <jimmyddao@gmail.com>
2025-07-12 18:00:34 -0400

    Add link to github

--- src/components/ProjectsSection.jsx
+++ src/components/ProjectsSection.jsx
@@ -1,4 +1,4 @@
-import { ExternalLink, Github } from "lucide-react";
+import { ArrowRight, ExternalLink, Github } from "lucide-react";
 
 const projects = [
   {
@@ -102,6 +102,16 @@ export const ProjectsSection = () => {
               </div>
             </div>
           ))}
+        </div>
+
+        <div className="text-center mt-12">
+          <a
+            href="https://github.com/Daojim"
+            target="_blank"
+            className="cosmic-button w-fit flex items-center mx-auto gap-2"
+          >
+            Check my GitHub <ArrowRight size={16} />
+          </a>
         </div>
       </div>
     </section>
