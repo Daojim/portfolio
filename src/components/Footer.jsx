3f5e72ff46e16e95ec19003c82ce5d98e53800cf
Daojim <jimmyddao@gmail.com>
2025-07-12 18:52:12 -0400

    Add footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,3 +1,15 @@
+import { ArrowUp } from "lucide-react";
+
 export const Footer = () => {
-  return <footer></footer>;
+  return (
+    <footer className="py-6 px-8 bg-card relative border-t border-border mt-6 pt-8 flex flex-wrap justify-between items-center">
+      <p className="text-sm"> &copy; {new Date().getFullYear()} Jimmy Dao</p>
+      <a
+        href="#hero"
+        className="p-2 rounded-full bg-primary/10 hover:bg-primary/20 text-primary transition-colors"
+      >
+        <ArrowUp size={20} />
+      </a>
+    </footer>
+  );
 };
